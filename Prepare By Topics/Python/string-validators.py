a69d02b4d91dc460cda57302225dcb0359f5c8aa
zidanebachtiar13 <zidanebachtiar13@gmail.com>
2022-12-02 11:01:47 +0700

    update python

---
+++ Prepare By Topics/Python/string-validators.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__':
+    s = input()
+
+    alnum = False
+    alpha = False
+    digit = False
+    lower = False
+    upper = False
+    for i in s:
+        if i.isalnum():
+            alnum = True
+        if i.isalpha():
+            alpha = True
+        if i.isdigit():
+            digit = True
+        if i.islower():
+            lower = True
+        if i.isupper():
+            upper = True
+    print(alnum)
+    print(alpha)
+    print(digit)
+    print(lower)
+    print(upper)
