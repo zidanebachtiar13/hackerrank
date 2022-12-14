b871dce0cb4ca38f21ce605a10c83dc663b369dc
zidanebachtiar13 <zidanebachtiar13@gmail.com>
2022-12-14 18:57:44 +0700

    update cpp

---
+++ Prepare By Topics/C++/c-tutorial-basic-data-types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+	int i;
+	long l;
+	char ch;
+	float f;
+	double d;
+	scanf("%d %ld %c %f %lf", &i, &l, &ch, &f, &d);
+	printf("%d\n", i);
+	printf("%ld\n", l);
+	printf("%c\n", ch);
+	printf("%f\n", f);
+	printf("%lf\n", d);
+	return 0;
+}
